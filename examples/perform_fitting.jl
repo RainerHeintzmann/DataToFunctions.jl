123e8f1d291575cd1e5e68a2116877029c854ef7
hzarei4 <hzoshtolagh@gmail.com>
2023-04-04 19:48:21 +0200

    Adding optim support in parallel

--- examples/perform_fitting.jl
+++ examples/perform_fitting.jl
@@ -1,26 +1,28 @@
 using DataToFunctions
 using Optim, StaticArrays, LinearAlgebra
 using Zygote
-using ForwardDiff,  LineSearches
+using ForwardDiff,  LineSearches, Plots, Printf
 
 
-true_vals = [0.0, 0.0, 1.0, 0.95]
+Base.show(io::IO, f::Float64) = @printf(io, "%.2f", f)
 
+true_vals =   [1.0, 2.0, 1.0, 1.0]
+init_x =      [0.2, 0.5, 1.0, 1.0]
 
 sample_data = rand(11,12)
 
 f = get_function(sample_data; super_sampling=2);
-fitting_data = f([0.0, 0.0], [1.0, 0.95])
+#f(p0::Vector{Float64}) = f([p0[1], p0[2], p0[3], p0[4]])
+
+fitting_data = f(true_vals) .+ rand(11, 12)./10.0
 #f(p2[1], p2[2]) = f(p2::Vector{Tuple{Float64, Float64}})
 loss(p, z) = sum(abs2.(f(p, z) .- fitting_data))
 loss(p2::Vector{Tuple{Float64, Float64}}) = loss(p2[1], p2[2])
 loss(p3) = loss([p3[1], p3[2]], [p3[3], p3[4]])
 
-initial_x = [(0.0, 0.0), (1.0, 1.0)]
 
-loss(initial_x)
 
-init_x = [0.0, 0.0, 1.1, 0.8]
+
 
 #Zygote.forwarddiff(loss, init_x)
 
@@ -41,25 +43,45 @@ GradientDescent(; alphaguess = 0.01,
        P = nothing,
        precondprep = (P, x) -> nothing
 )
-
-lower = [-10.0, -10.0, 0.00001, 0.00001]
-upper = [10.0, 10.0, 10.0, 10.0]
+"""
+lower = [-1*size(fitting_data)[1], -1*size(fitting_data)[2], 0.0, 0.0]
+upper = [size(fitting_data)[1], size(fitting_data)[2], size(fitting_data)[1], size(fitting_data)[2]]
 #initial_x = [2.0, 2.0]
 # requires using LineSearches
-inner_optimizer = LBFGS(; linesearch=LineSearches.BackTracking(order=2))
-res = optimize(loss, lower, upper, init_x, Fminbox(inner_optimizer), Optim.Options(store_trace = true, extended_trace = true, iterations=500), autodiff = :forward)
+inner_optimizer = LBFGS(; m=1, linesearch=LineSearches.BackTracking(order=2))
+res = optimize(
+       loss, 
+       lower, upper, 
+       init_x, 
+       Fminbox(inner_optimizer), 
+       #Optim.Options(store_trace = true, extended_trace = true, iterations=500), 
+       autodiff = :forward
+)
+
+
+
+
+heatmap(fitting_data .- f(Optim.x_trace(res)[end]), aspect_ratio=1, clim=(0.0, 1.0))
+
 """
 
-res = optimize(loss, init_x, LBFGS(), Optim.Options(store_trace=true, extended_trace=true, iterations=500), autodiff = :forward)
-trace = Optim.trace(res)
+res = optimize(
+       loss, init_x, 
+       LBFGS(), 
+       Optim.Options(store_trace=true, extended_trace=true, iterations=500), 
+       autodiff = :forward
+       )
+"""
 
+trace = Optim.trace(res);
+trace
 
 
 
 
 Optim.minimizer(res)
 Optim.f_trace(res)
-Optim.x_trace(res)[end]
+Optim.x_trace(res)
 Optim.converged(res)
 Optim.g_norm_trace(res)
 Optim.g_calls(res)
@@ -67,5 +89,19 @@ Optim.g_calls(res)
 loss(Optim.x_trace(res)[end])
 ForwardDiff.gradient(loss, Optim.x_trace(res)[end])
 
-Optim.trace(res)
 
+
+anim = @animate for i1 in 1:length(Optim.x_trace(res))
+
+       heatmap(fitting_data .- f(Optim.x_trace(res)[i1]), 
+              aspect_ratio=1, 
+              clim=(0.0, 1.0),
+              dpi=300
+       )
+       title!("iteration: $(Int(i1))/$(length(Optim.x_trace(res))),
+              estimation: $(Optim.x_trace(res)[i1])
+              true vals : $(true_vals)")
+
+end;
+
+gif(anim, "anim1.mp4", fps=5)
