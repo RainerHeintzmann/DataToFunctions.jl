308d69658f6daa44776c90ca14ee94cfdd3b4438
hzarei4 <hzoshtolagh@gmail.com>
2023-04-05 13:05:57 +0200

    adding comments and output plots

--- examples/perform_random_optim.jl
+++ examples/perform_random_optim.jl
@@ -4,20 +4,34 @@ using Zygote
 using ForwardDiff,  LineSearches, Plots, Printf
 using ProfileView, Profile
 
-
+# to show all the numbers in 2 decimals format
 Base.show(io::IO, f::Float64) = @printf(io, "%.2f", f)
 
 ###
+# creating the random matrix in size = (11, 12)
+sample_data = rand(11, 12)
 
-sample_data = rand(11,12)
+# creating the lower and upper bounds of the fitting variables (shift and scaling)
+#   shift can not be higher than the size of the array,
+#   scale can not be lower than zero or higher than size of the data, the latter causes the resulting array to be just one pixel
 lower = [-1*size(sample_data)[1], -1*size(sample_data)[2], 0.0, 0.0]
 upper = [size(sample_data)[1], size(sample_data)[2], size(sample_data)[1], size(sample_data)[2]]
 
+# preparing the function to fit
 f = get_function(sample_data; super_sampling=2);
+
+# assigning a scale (multiplier) to the range of true values, wedo not want that the parameters of 
+# function to be near the limits and cause strange behavior of the optimization
 scale_range = 4.0
+
+# true values of the fitting are random for the repeatibility
 true_vals =  (rand(4) .* (upper .- lower)/scale_range ) .+ (lower/scale_range) #[3.0, 5.5, 1.85, 0.6]
+
+# create the fitting data and adding random noise with scale of 1/10
 fitting_data = f(true_vals) .+ rand(11, 12)./10.0
 
+# create the loss function and using the Julia's multiple dispatch
+# because the gradients function is required just one input (can be vector)
 loss(p, z) = sum(abs2.(f(p, z) .- fitting_data))
 loss(p2::Vector{Tuple{Float64, Float64}}) = loss(p2[1], p2[2])
 loss(p3) = loss([p3[1], p3[2]], [p3[3], p3[4]])
@@ -25,45 +39,71 @@ loss(p3) = loss([p3[1], p3[2]], [p3[3], p3[4]])
 
 
 
-#initial_x = [2.0, 2.0]
-# requires using LineSearches
+# initialization of the LBFGS optimizer
 inner_optimizer = LBFGS(; m=1, linesearch=LineSearches.BackTracking(order=2))
 
 
 
 function perform_optim_mthr(loss, n_walkers)
-    est = zeros(n_walkers, 5)
+    """
+        defining a function to survey the parameter space to neglect the local 
+        minima and find the global maxima
+
+        loss: the loss function
+        n_walkers: number of random initial parameter estimation
+    """
+    # allocating the estimation array: 
+    # consists of four parameters [1:4] and the minimum loss function of them [5]
+    est_m = zeros(n_walkers, 5)
+
     #x_tr = Array{Any}
     #res = Array{Optim.MultivariateOptimizationResults{}}(undef, n_walkers, 1)
     #res = Optim.MultivariateOptimizationResults{}
-
+    
+    # defining the random initial parameter values
     walkers = (rand(4, n_walkers) .* (upper .- lower)/scale_range ) .+ (lower/scale_range)
+    
+    # main loop to do the optimization for each of the initial parameter values
+    # it uses the Threads to distribute the for loop to each thread
+    # note that in the settings.json the Julia is started with 16 threads
     Threads.@threads for i in 1:size(walkers)[2]
         res = optimize(
             loss,
-            lower, upper, 
+            lower, upper, # the limits (simple box constraints)
             walkers[:, i], 
-            Fminbox(inner_optimizer), 
-            #Optim.Options(store_trace = true, extended_trace = true, iterations=500), 
+            Fminbox(inner_optimizer),  # assigning the limits of fitting (simple constraints) along with the LBFGS
+            Optim.Options(store_trace = true, extended_trace = true, iterations=500), 
             autodiff = :forward
         );
         #x_tr = Optim.x_trace(res)
 
-        est[i, 1:4] .= Optim.minimizer(res)#x_tr[end]
-        est[i, 5] = minimum(res)#loss(x_tr[end])
+        # saving each 4 parameters of the fit and the minimum loss function
+        est_m[i, 1:4] .= Optim.minimizer(res)#x_tr[end]
+        est_m[i, 5] = minimum(res)#loss(x_tr[end])
     end
-    ans1 = est[argmin(est[:, 5]), :]
-    return est, ans1
+    # saving the parameters of the  minimum of the loss function 
+    ans_m = est_m[argmin(est_m[:, 5]), :]
+    return est_m, ans_m
 end
 
 #@profile est_m, ans_m = perform_optim_mthr(loss, 10000)
 
 
-@time est_m, ans_m = perform_optim_mthr(loss, 20000); #  5.191172 seconds (126.96 M allocations: 26.015 GiB, 54.49% gc time, 0.90% compilation time)
+@time est_m, ans_m = perform_optim_mthr(loss, 5000); #  2.374773 seconds (59.62 M allocations: 11.897 GiB, 52.04% gc time, 4.47% compilation time: 42% of which was recompilation))
 
 println(string(true_vals) * "\n" * string(ans_m))
 # [1.07, -2.63, 0.78, 1.65]
 # [1.02, -2.60, 0.77, 1.66, 0.46]
+
+p00 = heatmap(sample_data, aspect_ratio=1.0, clim=(0.0, 1.0), title="Sample data", legend = :none);
+p01 = heatmap(fitting_data, aspect_ratio=1.0, clim=(0.0,1.0), title="Fitting data", legend = :none);
+p02 = heatmap(f(ans_m[1:4]), aspect_ratio=1.0, clim=(0.0,1.0), title="estimated fit", legend = :none);
+p03 = heatmap(fitting_data .- f(ans_m[1:4]), aspect_ratio=1.0, clim=(0.0,1.0), title="discrepancy", legend = :none);
+
+plot(p00, p01, p02, p03, layout=@layout([A B C D]), framestyle=nothing, showaxis=false, xticks=false, yticks=false, size=(700, 300))
+savefig("Output_mth_1.png")
+
+
 
 
 function perform_optim_sthr(loss, n_walkers=1000)
@@ -88,7 +128,7 @@ function perform_optim_sthr(loss, n_walkers=1000)
 end
 
 
-@time est_s, ans_s = perform_optim_sthr(loss, 20000); # 17.400975 seconds (105.36 M allocations: 21.475 GiB, 7.54% gc time)
+@time est_s, ans_s = perform_optim_sthr(loss, 20000); 
 
 println(string(true_vals) * "\n" * string(ans_s))
 # [1.07, -2.63, 0.78, 1.65]
