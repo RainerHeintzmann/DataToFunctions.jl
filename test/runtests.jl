de4427b6c01024abb1b564bf8afcb1cb22cc21c7
RainerHeintzmann <heintzmann@gmail.com>
2023-04-12 21:13:41 +0200

    fixed scaling problem

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,6 @@
 using Test
-using Zygote
+# using Zygote
+using DataToFunctions
 
 @testset "get_function" begin
     data = rand(40,41)
@@ -16,6 +17,15 @@ end
     @test loss((0.0,0.0),(1.0,1.0)) < 1e-20
     @test loss((0.0,0.001),(1.0,1.0)) > 1e-20
     @test loss((0.0,0.0),(1.0001,1.0)) > 1e-20
+
+    # throws an error...
+    # Zygote.gradient(loss, (0.0,0.0), (1.0,1.0))
+end
+
+@testset "keep center" begin
+    data = ones(5,4); data[3,3] = 5.0;
+    f = get_function(data; super_sampling=5);
+    @test f((0.0,0.0),(2.0,2.0))[3,3] ≈ 5.0
 
     # throws an error...
     # Zygote.gradient(loss, (0.0,0.0), (1.0,1.0))
