ee0db0ad86d170cd4442de3f34e01acac49f3137
RainerHeintzmann <heintzmann@gmail.com>
2023-04-13 07:18:39 +0200

    corrected resample problem. Now it is centered. Also changed the default extrapolation_bc to 0.0

--- src/DataToFunctions.jl
+++ src/DataToFunctions.jl
@@ -12,17 +12,18 @@ This is useful for fitting with a function which is itself defined by measured d
 # Arguments
 `data`: The data to represent by the function `dat`
 `super_sampling`: The factor by which the data is internally represented as a supersampled version (Fourier-based upsampling, see `FourierTools.resample`)
-`extrapolation_bc`: The extrapolation boundary condition to select for values outside the range. See the package `Interpolation` for details.
+`extrapolation_bc`: The extrapolation boundary condition to select for values outside the range. 
+    By default the value 0.0 is used. Other options are `Flat()`, or `Line()`, See the package `Interpolation` for details.
 `interp_type`: The type of interpolation to use. See the package `Interpolation` for details.
 
 # Example
 ```jldoctest
 ```
 """
-function get_function(data::AbstractArray; super_sampling=2, extrapolation_bc=Flat(), interp_type=Interpolations.BSpline(Linear()))
+function get_function(data::AbstractArray; super_sampling=2, extrapolation_bc=zero(eltype(data)), interp_type=Interpolations.BSpline(Linear()))
     new_size = super_sampling.*size(data)
-    upsampled = resample(data, new_size)
-    # @show upsampled
+    upsampled = fftshift(resample(ifftshift(data), new_size))
+    @show upsampled
     # return upsampled
     # itp = LinearInterpolation(axes(upsampled), upsampled, extrapolation_bc=extrapolation_bc);
     interpolation = Interpolations.interpolate(upsampled, interp_type)
@@ -35,7 +36,7 @@ function get_function(data::AbstractArray; super_sampling=2, extrapolation_bc=Fl
     function zoomed(shift, zoom)
         zoom = zoom .* super_sampling
         # careful: The center of the original data is not at the expected position! But rather at:
-        center_upsamp = ((center_orig .-1) .*super_sampling .+1)  # new_size .÷2 .+1
+        center_upsamp = new_size .÷2 .+1 # ((center_orig .-1) .*super_sampling .+1)  # new_size .÷2 .+1
         scaled_axes = ((ax.-myc) .* z .+ cen for (ax, myc, cen, z) in zip(zero_axes, shift, center_upsamp, zoom))
         # @show Tuple(scaled_axes)
         return interpolation[scaled_axes...]
