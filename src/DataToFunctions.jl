7ecba4b315f790d38529a9fb7cea04328384c69f
hzarei4 <hzoshtolagh@gmail.com>
2023-05-31 17:47:38 +0200

    added the general 7 parameters fitting

--- src/DataToFunctions.jl
+++ src/DataToFunctions.jl
@@ -1,7 +1,12 @@
 module DataToFunctions
 using Interpolations
 using FourierTools
-export get_function
+using Optim, LineSearches
+using Revise
+using StaticArrays
+using Zygote
+
+export get_function, perform_fit, get_function_general, perform_fit_general
 
 """
     get_function(data::AbstractArray; super_sampling=2, extrapolation_bc=Flat(), interp_type=Interpolations.BSpline(Linear()))
@@ -23,7 +28,7 @@ This is useful for fitting with a function which is itself defined by measured d
 function get_function(data::AbstractArray; super_sampling=2, extrapolation_bc=zero(eltype(data)), interp_type=Interpolations.BSpline(Linear()))
     new_size = super_sampling.*size(data)
     upsampled = fftshift(resample(ifftshift(data), new_size))
-    @show upsampled
+    # @show upsampled
     # return upsampled
     # itp = LinearInterpolation(axes(upsampled), upsampled, extrapolation_bc=extrapolation_bc);
     interpolation = Interpolations.interpolate(upsampled, interp_type)
@@ -45,6 +50,7 @@ function get_function(data::AbstractArray; super_sampling=2, extrapolation_bc=ze
 
     return zoomed
 
+    zoomed(p) = zoomed([p[1], p[2]], [p[3], p[4]])
     # return (pos) -> interp_linear((center .+ pos)...)
     # fitp(t) = interp_linear(t...)
     # @time res1 = fitp.(tcoords);  # 1 sec
@@ -97,6 +103,97 @@ function perform_fit(loss_function, fitting_data::AbstractArray)
     
     # return the estimated parameters
     return Optim.minimizer(res)
+end
+
+
+
+function get_function_general(data::AbstractArray; super_sampling=1)
+    # new_size = super_sampling.*size(data)
+    # upsampled = fftshift(resample(ifftshift(data), new_size))
+
+    itp = extrapolate(interpolate(data, BSpline(Linear())), 0.0);
+
+    function f(t::SVector, matrix_c::SMatrix) 
+        return matrix_c * t
+    end
+
+    function interpolated(p)
+        
+        out = similar(data)
+        x_tmp = Zygote.Buffer(out)
+
+        x_cen, y_cen = (size(data) .÷ 2.0 .+1)
+        # x_cen_up, y_cen_up = (size(upsampled) .÷ 2.0 .+ 1.0)
+
+        # scale_x *= super_sampling
+        # scale_y *= super_sampling
+        # @show p[1]
+        rot_mat =  @SMatrix [cos(p[7])  -1.0*sin(p[7]) 0.0; sin(p[7])  cos(p[7]) 0.0; 0.0 0.0 1.0];
+        shear_mat = @SMatrix [1.0 p[5] 0.0; p[6] 1.0 0.0; 0.0 0.0 1.0];
+        scale_mat = @SMatrix [1/p[3] 0.0 0.0; 0.0 1/p[4] 0.0; 0.0 0.0 1.0];
+        shift_mat = @SMatrix [1.0 0.0 -1*p[1]; 0.0 1.0 -1*p[2]; 0.0 0.0 1.0];
+        t_to_origin = @SMatrix [1.0 0.0 1*x_cen; 0.0 1.0 y_cen; 0.0 0.0 1.0];
+        t_to_center = @SMatrix [1.0 0.0 -1.0*x_cen; 0.0 1.0 -1.0*y_cen; 0.0 0.0 1.0];
+        # t_orig_upsampled = @SMatrix [1.0 0.0 -1.0*x_cen_up; 0.0 1.0 -1.0*y_cen_up; 0.0 0.0 1.0]
+
+        matrix_c = t_to_origin * scale_mat * shear_mat * rot_mat *shift_mat * t_to_center
+
+        
+
+        for I in CartesianIndices(data)
+            x_tmp[I] = itp(f(SVector(Tuple(I)..., 1), matrix_c)[1:2]...)
+        end
+        
+        return copy(x_tmp)
+    end
+
+    return interpolated
+end
+
+
+"""
+    perform_fit(loss_function, fitting_data::AbstractArray)
+
+Performs a fit to the fitting data using a loss function defined by the user
+
+# Arguments
+`loss_function`: User-defined loss function which is minimized
+`fitting_data`: The data which is being fitted 
+
+# Returns
+a vector of 4 parameters: first 2 for the shift and other 2 for the scaling factors
+
+# Example
+there is an example of this function in the `examples/star_fitting.jl`
+
+"""
+function perform_fit_general(loss_function, fitting_data::AbstractArray)
+    # guess the shift parameters by taking the maximum values of the array and
+    # centering the positions
+    a, b = Tuple(argmax(fitting_data)) .- size(fitting_data) ./2.0 .- 1.0
+    
+    # assigning the initial parameter estimates
+    init_x = [a, b, 1.0, 1.0, 0.0, 0.0, 0.0]
+
+    # setting the lower and upper boundary of the parameter values based on the limits of the shift and scaling
+    lower = [-1*size(fitting_data)[1], -1*size(fitting_data)[2], 0.0, 0.0, 0.0, 0.0, 0.0]
+    upper = [size(fitting_data)[1], size(fitting_data)[2], size(fitting_data)[1], size(fitting_data)[2], 5.0, 5.0, pi]
+
+    # initializing the LBFGS optimizer
+    inner_optimizer = LBFGS(; m=1, linesearch=LineSearches.BackTracking(order=2))
+    
+    # Computer, Optimize! :D
+    res = optimize(
+            loss_function, 
+            lower, upper, 
+             init_x, 
+            Fminbox(inner_optimizer), 
+            Optim.Options(store_trace = true, extended_trace = true, iterations=500), 
+            autodiff = :finite
+        )
+    
+    # return the estimated parameters
+    return Optim.minimizer(res), res
 end
 
 end # module DataToFunctions
