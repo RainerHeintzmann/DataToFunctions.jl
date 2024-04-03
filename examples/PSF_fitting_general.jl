63f548c3515e63feb5f143aeecbe3c93ec7ddf10
RainerHeintzmann <heintzmann@gmail.com>
2024-04-03 12:18:07 +0200

    rewrote some bits towards modularity

--- examples/PSF_fitting_general.jl
+++ examples/PSF_fitting_general.jl
@@ -9,11 +9,6 @@ using TestImages
 
 
 import Random
-Random.seed!(1234)
-
-Base.show(io::IO, f::Float64) = @printf(io, "%.3f", f)
-
-
 
 """
     perform_fit_general(loss_function, fitting_data::AbstractArray)
@@ -110,10 +105,12 @@ end
 
 
 
-
+function main()
+Random.seed!(1234)
+# Base.show(io::IO, f::Float64) = @printf(io, "%.3f", f)
+    
 # size of the test array to fit
 size_arr = 22
-
 noise_level = 10.0;
 
 # defining the mean and the varixance of the test normal (Gaussian) distribution
@@ -128,7 +125,6 @@ noise_level = 10.0;
 p = MvNormal(μ, Σ)
 
 
-
 # this part of the code is to define the sample array based on a 2D normal distribution
 X = -1*size_arr/2.0:1*size_arr/2.0
 Y = -1*size_arr/2.0:1*size_arr/2.0 
@@ -140,7 +136,7 @@ z = [pdf(p, [x,y]) for y in Y, x in X]
 dtype = Float64
 
 # setting a typical values for the shift (1:2) and scale (3:4)
-true_vals =   dtype.([0.2, -1.2, 1.0, 1.0, 0.0, 0.0, pi/3])#pi/6]
+true_vals =   dtype.([0.2, -1.2, 1.0, 1.0, 0.0, 0.0, pi/3]) #pi/6]
 #true_vals =   [2.3, -1.2, 0.9, 2.1, 0.1, 0.05, pi/2, 2.0, 3.0]
 
 # normalizing the sample data
@@ -149,8 +145,6 @@ sample_data = dtype.(TestImages.shepp_logan(32));
 #sample_data = rand(dtype, (size_arr, size_arr))
 
 #sample_data .+=  rand(dtype, (size(sample_data)...))./noise_level;
-
-
 
 x_cen, y_cen = (size(sample_data) .÷ 2.0 .+1)
 
@@ -161,8 +155,7 @@ t_to_origin = [1.0 0.0 1*x_cen; 0.0 1.0 y_cen; 0.0 0.0 1.0];
 t_to_center = [1.0 0.0 -1.0*x_cen; 0.0 1.0 -1.0*y_cen; 0.0 0.0 1.0];
 
 # converting the data to function (DataToFunctions.get_function)
-f_general = get_function_affine(sample_data);#; super_sampling=1);#, extrapolation_bc=0.0);
-
+f_affine = get_function_affine(sample_data);#; super_sampling=1);#, extrapolation_bc=0.0);
 
 ang = rand((0.0:0.1:pi))
 rot_mat =  [cos(ang)  -1.0*sin(ang) 0.0; sin(ang)  cos(ang) 0.0; 0.0 0.0 1.0];
@@ -173,14 +166,14 @@ matrix_c = dtype.(t_to_origin * scale_mat * shear_mat * rot_mat * t_to_center )
 
 # adding some scaled random noise to the fitting data
 # fitting_data = f_general(SMatrix{3,3}(matrix_c)); #.+ rand(size(sample_data)...)./100.0;
-fitting_data = f_general(true_vals) .+ dtype.(rand(size(sample_data)...))./5.0;
+fitting_data = f_affine(true_vals) .+ dtype.(rand(size(sample_data)...))./5.0;
 
 
 plot(heatmap(sample_data, aspect_ratio=1), heatmap(fitting_data, aspect_ratio=1))
 
 
 # defining the loss function based on the gaussian noise
-loss(p1::AbstractVector) = sum(abs2.(f_general(p1::AbstractVector) .- fitting_data))
+loss(p1::AbstractVector) = sum(abs2.(f_affine(p1::AbstractVector) .- fitting_data))
 # loss(x) = loss(x::AbstractVector{T} where T)
 
 # loss(p3) = loss([p3[1], p3[2], p3[3], p3[4], p3[5], p3[6], p3[7]])
@@ -191,22 +184,23 @@ loss(p1::AbstractVector) = sum(abs2.(f_general(p1::AbstractVector) .- fitting_da
 
 
 # plotting the output of the fitting pocedure for further illustration
-begin
-    p00 = heatmap(sample_data, aspect_ratio=1.0, clim=(0.0, 1.0), title="Sample data", legend = :none);
-    p01 = heatmap(fitting_data, aspect_ratio=1.0, clim=(0.0,1.0), title="Fitting data", legend = :none);
-    p02 = heatmap(f_general(output), aspect_ratio=1.0, clim=(0.0,1.0), title="estimated fit", legend = :none);
-    p03 = heatmap(fitting_data .- f_general(output), aspect_ratio=1.0, clim=(0.0, 1.0), title="discrepancy", legend = :none);
+    begin
+        p00 = heatmap(sample_data, aspect_ratio=1.0, clim=(0.0, 1.0), title="Sample data", legend = :none);
+        p01 = heatmap(fitting_data, aspect_ratio=1.0, clim=(0.0,1.0), title="Fitting data", legend = :none);
+        p02 = heatmap(f_affine(output), aspect_ratio=1.0, clim=(0.0,1.0), title="estimated fit", legend = :none);
+        p03 = heatmap(fitting_data .- f_affine(output), aspect_ratio=1.0, clim=(0.0, 1.0), title="discrepancy", legend = :none);
 
-    plot(p00, p01, p02, p03, layout=@layout([A B C D]), 
-        framestyle=nothing, showaxis=false, 
-        xticks=false, yticks=false, 
-        size=(1200, 500),  
-        plot_title="True vals: $(true_vals)
-  fitted vals: $(output)",
-        plot_titlevspan=0.2
-    )
+        plot(p00, p01, p02, p03, layout=@layout([A B C D]), 
+            framestyle=nothing, showaxis=false, 
+            xticks=false, yticks=false, 
+            size=(1200, 500),  
+            plot_title="True vals: $(true_vals)
+    fitted vals: $(output)",
+            plot_titlevspan=0.2
+        )
+    end
+
 end
-
 
 
 
--- src/DataToFunctions.jl
+++ src/DataToFunctions.jl
@@ -3,7 +3,7 @@ using Interpolations
 using FourierTools
 using StaticArrays
 
-export get_function, get_function_affine, add_dim, red_dim_apply, red_dim, f, func_transform
+export get_function, get_function_affine, add_dim, red_dim_apply, red_dim, mat_mul, func_transform
 export extrapolate, interpolate
 
 """
@@ -60,16 +60,20 @@ function add_dim(cind)
     return SVector.((Tuple(cind))..., 1)
 end
 
-@inline function red_dim_apply(fct::AbstractArray{R}, svec::SVector{S,T})::R where {S,T, R}
-    return fct((@view svec[1:2])...)
-end
-
 @inline function red_dim(svec::SVector{S,T})::SVector{S-1,T} where {S,T}
     return @view svec[1:S-1]
 end
 
+@inline function red_dim_apply(fct::AbstractArray{R}, svec::SVector{S,T})::R where {S,T, R}
+    return fct((@view svec[1:S-1])...)
+end
+
+@inline function idx_apply(fct::AbstractArray{R}, svec::SVector{S,T})::R where {S,T, R}
+    return fct(svec...)
+end
+
 # multiplying the transformation matrix
-@inline function f(t::SVector{N, Int64}, matrix_c::SMatrix{N,N,T})::SVector{N,T} where {N,T}
+@inline function mat_mul(t::SVector{N, Int64}, matrix_c::SMatrix{N,N,T})::SVector{N,T} where {N,T}
     return matrix_c * t
 end
 
@@ -80,11 +84,74 @@ end
 end
 
 
+# How to run this function:
+# f_general((t) -> (t[1]*1.01, t[2]*1.01), out2)
+"""
+    apply_transform_tuple!(coord_transf_func::Function, data, itp, out)
+
+applies a tuple-based coordinate transformation function to the indices of an array and returns the transformed array
+
+coord_transf_func: A function that takes a tuple of coordinates and returns a new tuple of coordinates
+data: The data to transform
+itp: The interpolation object to use
+
+# Example
+```jldoctest
+```
+"""
+function apply_transform_tuple!(coord_transf_func::Function, data, itp, out)
+    out .= red_dim_apply.(Ref(itp), func_transform.(CartesianIndices(data), Ref(coord_transf_func)));
+end
+
+"""
+    apply_transform_svec!(coord_transf_func::Function, data, itp, out)
+applies a homogeneous coordinate-based coordinate transformation function to the indices of an array and returns the transformed array
+
+coord_transf_func: A function that takes a N-+1 dimensional homogeneous SVector returns a new N+1 dimensional SVector
+data: The data to transform
+itp: The interpolation object to use
+
+"""
+function apply_transform_svec!(coord_transf_func::Function, data, itp, out)
+    out .= idx_apply.(Ref(itp), coord_transf_func.(CartesianIndices(data)));
+end
+
+"""
+    apply_transform_homogen!(coord_transf_func::Function, data, itp, out)
+applies a homogeneous coordinate-based coordinate transformation function to the indices of an array and returns the transformed array
+
+coord_transf_func: A function that takes a N-+1 dimensional homogeneous SVector returns a new N+1 dimensional SVector
+data: The data to transform
+itp: The interpolation object to use
+
+"""
+function apply_transform_homogen!(coord_transf_func::Function, data, itp, out)
+    h_coord_transf_func = (c) -> red_dim(coord_transf_func(add_dim(c)))
+    apply_transform_svec!(h_coord_transf_func, data, itp, out);
+    # out .= itp.(red_dim.(coord_transf_func.(add_dim.(CartesianIndices(data)))));
+    # out .= red_dim_apply.(Ref(itp), coord_transf_func.(add_dim.(CartesianIndices(data))));
+end
+
+function apply_transform_affine!(mymat, data, itp, out)
+    # red_dim_apply.(Ref(itp), func_transform.(CartesianIndices(data), Ref(coord_transf_func)));
+    # return red_dim_apply.(Ref(itp), mat_mul.(add_dim.(CartesianIndices(data)), Ref(mymat)));
+    
+    homogenous_transform = (c) -> mat_mul(c, mymat)
+    apply_transform_homogen!(homogenous_transform, data, itp, out);
+    #return out
+end
+
+function get_function_homogen(data::AbstractArray{T}, fct_hom::Function; super_sampling=2, extrapolation_bc=zero(eltype(data)), interp_type=Interpolations.BSpline(Linear())) where T
+end
+
 """
     get_function_affine(data::AbstractArray; super_sampling=1, extrapolation_bc=Flat(), interp_type=Interpolations.BSpline(Linear()))
 
-returns a function `interpolated(p)` which generates a transformed version of the original data. 
+returns a function `interpolated(p, [out])` which generates a transformed version of the original data parameterized by transform parameters.
 This is useful for fitting with a function which is itself defined by measured data.
+The returned function supports two ways to be used, with an affine transform matrix `p` as in input or with a vector `p` of parameters. 
+The optional argument `out` can be used to store the result of the transformation.
+
 
 # Arguments
 `data`: The data to represent by the function `dat`
@@ -101,47 +168,30 @@ function get_function_affine(data::AbstractArray{T}; super_sampling=2, extrapola
     itp = extrapolate(interpolate(data, interp_type), extrapolation_bc);
 
 
-    function interpolated(matrix_c::SMatrix)
+    @inline function interpolated(matrix_c::SMatrix, out = similar(data))
         
-        # init a new array for the output
-        out = data
+        # # init a new array for the output
+        # out = similar(data)
+        # out = data
 
-        # x_cen, y_cen = (size(data) .÷ 2.0 .+1)
-
-        # t_orig_upsampled = @SMatrix [1.0 0.0 -1.0*x_cen_up; 0.0 1.0 -1.0*y_cen_up; 0.0 0.0 1.0]
-
-        # building the overall transformation matrix
-        #   matrix_c = SMatrix{ndims(data)+1, ndims(data)+1, T}(reshape(p, ndims(data)+1, ndims(data)+1))
+        # out[CartesianIndices(data)] .= red_dim_apply.(Ref(itp), mat_mul.(add_dim.(CartesianIndices(data)), Ref(matrix_c)));
+        return apply_transform_affine!(matrix_c, data, itp, out);
+        # return red_dim_apply.(Ref(itp), f.(add_dim.(CartesianIndices(data)), Ref(matrix_c)));
         
-        #print(eltype(matrix_c))
-        # looping all over the catesian indedices of the input image, 
-        # first ading a new value to its third dimenstion: 1.0,
-        # converting to the new indices using the transformation matrix and then,
-        # using the "itp" object, we build the transfomed image "out"
-        
-        #for I1 in CartesianIndices(data)
-            #print("INSIde the loop!!")
-            #print(eltype(SVector{ndims(data)+1, T}(Tuple(I1)..., 1)))
-        #    out[I1] = itp(f(SVector{ndims(data)+1, T}(Tuple(I1)..., 1.0), matrix_c)[1:2]...)
-            #print(eltype(out[I1]))
-        #end
-
-        out[CartesianIndices(data)] .= red_dim_apply.(Ref(itp), f.(add_dim.(CartesianIndices(data)), Ref(matrix_c)));
-        
-        return out
+        # return out
     end
 
-    function interpolated(matrix_c::SMatrix, out::AbstractArray)
+    # function interpolated!(matrix_c::SMatrix, out::AbstractArray)
 
-        out[CartesianIndices(data)] .= red_dim_apply.(Ref(itp), f.(add_dim.(CartesianIndices(data)), Ref(matrix_c)));
+    #     out[CartesianIndices(data)] .= red_dim_apply.(Ref(itp), f.(add_dim.(CartesianIndices(data)), Ref(matrix_c)));
         
-        #return out
-    end
+    #     #return out
+    # end
 
-    function interpolated(p::AbstractVector{T}) where T     
+    @inline function interpolated(p::AbstractVector{T}, out = similar(data)) where T     
 
         # init a new array for the output
-        out = data
+        # out = data
 
         x_cen, y_cen = (size(data) .÷ 2.0 .+1)
         # x_cen_up, y_cen_up = (size(upsampled) .÷ 2.0 .+ 1.0)
@@ -168,42 +218,34 @@ function get_function_affine(data::AbstractArray{T}; super_sampling=2, extrapola
         #    out[I1] = itp(f(SVector(Tuple(I1)..., 1), matrix_c)[1:2]...)
         #end
 
-        out[CartesianIndices(data)] .= red_dim_apply.(Ref(itp), f.(add_dim.(CartesianIndices(data)), Ref(matrix_c)));
+        # out[CartesianIndices(data)] .= red_dim_apply.(Ref(itp), f.(add_dim.(CartesianIndices(data)), Ref(matrix_c)));
+        return interpolated(matrix_c, out);
+        # return red_dim_apply.(Ref(itp), f.(add_dim.(CartesianIndices(data)), Ref(matrix_c)));
 
-        return out
+        # return out
     end
 
     
-    function interpolated(p::AbstractVector{T}, out::AbstractArray) where T     
+    # function interpolated(p::AbstractVector{T}, out::AbstractArray) where T     
 
-        x_cen, y_cen = (size(data) .÷ 2.0 .+1)
-        # x_cen_up, y_cen_up = (size(upsampled) .÷ 2.0 .+ 1.0)
+    #     x_cen, y_cen = (size(data) .÷ 2.0 .+1)
+    #     # x_cen_up, y_cen_up = (size(upsampled) .÷ 2.0 .+ 1.0)
 
-        # creating the matrices of rotation, shear, scale, and shift
-        rot_mat =  @SMatrix [cos(p[7])  -1.0*sin(p[7]) 0.0; sin(p[7])  cos(p[7]) 0.0; 0.0 0.0 1.0];
-        shear_mat = @SMatrix [1.0 p[5] 0.0; p[6] 1.0 0.0; 0.0 0.0 1.0];
-        scale_mat = @SMatrix [1/p[3] 0.0 0.0; 0.0 1/p[4] 0.0; 0.0 0.0 1.0];
-        shift_mat = @SMatrix [1.0 0.0 -1*p[1]; 0.0 1.0 -1*p[2]; 0.0 0.0 1.0];
-        t_to_origin = @SMatrix [1.0 0.0 1*x_cen; 0.0 1.0 y_cen; 0.0 0.0 1.0];
-        t_to_center = @SMatrix [1.0 0.0 -1.0*x_cen; 0.0 1.0 -1.0*y_cen; 0.0 0.0 1.0];
-        # t_orig_upsampled = @SMatrix [1.0 0.0 -1.0*x_cen_up; 0.0 1.0 -1.0*y_cen_up; 0.0 0.0 1.0]
+    #     # creating the matrices of rotation, shear, scale, and shift
+    #     rot_mat =  @SMatrix [cos(p[7])  -1.0*sin(p[7]) 0.0; sin(p[7])  cos(p[7]) 0.0; 0.0 0.0 1.0];
+    #     shear_mat = @SMatrix [1.0 p[5] 0.0; p[6] 1.0 0.0; 0.0 0.0 1.0];
+    #     scale_mat = @SMatrix [1/p[3] 0.0 0.0; 0.0 1/p[4] 0.0; 0.0 0.0 1.0];
+    #     shift_mat = @SMatrix [1.0 0.0 -1*p[1]; 0.0 1.0 -1*p[2]; 0.0 0.0 1.0];
+    #     t_to_origin = @SMatrix [1.0 0.0 1*x_cen; 0.0 1.0 y_cen; 0.0 0.0 1.0];
+    #     t_to_center = @SMatrix [1.0 0.0 -1.0*x_cen; 0.0 1.0 -1.0*y_cen; 0.0 0.0 1.0];
+    #     # t_orig_upsampled = @SMatrix [1.0 0.0 -1.0*x_cen_up; 0.0 1.0 -1.0*y_cen_up; 0.0 0.0 1.0]
 
-        # building the overall transformation matrix
-        # matrix_c = t_to_origin * scale_mat * shear_mat * rot_mat *shift_mat * t_to_center
-        matrix_c = t_to_origin * scale_mat * rot_mat *shift_mat * t_to_center
+    #     # building the overall transformation matrix
+    #     # matrix_c = t_to_origin * scale_mat * shear_mat * rot_mat *shift_mat * t_to_center
+    #     matrix_c = t_to_origin * scale_mat * rot_mat *shift_mat * t_to_center
 
-        out[CartesianIndices(data)] .= red_dim_apply.(Ref(itp), f.(add_dim.(CartesianIndices(data)), Ref(matrix_c)));
-    end
-
-    
-    # How to run this function:
-    # f_general((t) -> (t[1]*1.01, t[2]*1.01), out2)
-    function interpolated(coord_transf_func::Function, out::AbstractArray)
-
-        out[CartesianIndices(data)] .= red_dim_apply.(Ref(itp), func_transform.(CartesianIndices(data), Ref(coord_transf_func)));
-        #return out
-    end
-
+    #     out[CartesianIndices(data)] .= red_dim_apply.(Ref(itp), f.(add_dim.(CartesianIndices(data)), Ref(matrix_c)));
+    # end
 
     return interpolated
 end
