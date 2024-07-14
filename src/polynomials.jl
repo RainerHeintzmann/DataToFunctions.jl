9c2c7f75e818293ff5b607e1f923b295a222ef6c
RainerHeintzmann <heintzmann@gmail.com>
2024-07-14 21:01:55 +0200

    finally got it working fast

--- src/polynomials.jl
+++ src/polynomials.jl
@@ -11,49 +11,86 @@ Create a polynomial of order 0 with numvars variables.
 returned is a function that takes a tuple of variables and a tuple of coefficients and returns the value of the polynomial
     and the number of coefficients required (here 1).
 """
-# function get_polynomial(::Val{numvars}, ::Val{0}) where {numvars}
-#     # @info "Creating polynomials of order 0"
-function polynomial(::Val{0}, ::T1,  c::T2) where {T1 <: NTuple, T2 <: NTuple}
+function polynomial(::Val{0}, ::T1,  c::T2, ::Val{cstart}=Val(1), ::Val{numvars}=Val(1)) where {NV, TS, T1 <: NTuple{NV, Integer}, T2 <: NTuple{TS, Float32}, cstart, numvars}
     # println("c: $(c) $(length(c))");
-    return c[1] 
+    return c[cstart]
 end  #, (t,c) -> ntuple(n->c[1], Val(numvars))
-#     return myconst
-# end
 
 """
-    get_polynomial(::Val{numvars}, ::Val{N}) where {numvars, N}
+    polynomial(::Val{N}, t::T1, c::T2, ::Val{cstart}=Val(1), ::Val{numvars}=Val(length(t)))::Float32 where {N, NV, TS, T1 <: NTuple{NV, Integer}, T2 <: NTuple{TS, Float32}, cstart, numvars}
 
-Create a polynomial of order N with numvars variables.
-returned is a function that takes a tuple of variables and a tuple of coefficients and returns the value of the polynomial.
+Represents a polynomial of order N with numvars variables (also implicitely defined via the length of the NTuple `t`).
+Note that `numvars` is needed for the internal workings of the polynomial generator, but notmally not by the user.
 
 E.g. to represent a polynomial of order 1 with 2 variables, the coefficients are ordered as follows:
 c = (c0, c1, c2) where the polynomial is: c0 + c1*x + c2*y
 or for a polynomial of order 2 with 2 variables:
 c = (c0, c1, c2, c3, c4, c5) where the polynomial is c0 + c1*x + c2*x^2 + c3*y + c4*x*y + c5*y^2
 Note that the coefficients are ordered not by the multiples in which they appear in the polynomial, but by the order of the variables.
-"""
-# function get_polynomial(::Val{numvars}, ::Val{N}) where {numvars, N}
-#     # @info "Creating polynomials with $(numvars) variables of order , $(N). Required constants: $((numvars+1)^N)"
 
-function polynomial(::Val{N}, t::T1, c::T2)::Float32 where {N, T1<:NTuple, T2<:NTuple} # :: NTuple{NV, Float32}, NTuple{M, Float32}
-    c_start = 1
+Example:
+```jldoctest
+>julia polynomial(Val(2), (2, 20),  (1f0, 1f0, 1f0, 1f0, 1f0, 1f0))
+467.0f0
+>julia cs = Tuple(Float32.(collect(1:27)))
+  (1.0f0, 2.0f0, 3.0f0, 4.0f0, 5.0f0, 6.0f0, 7.0f0, 8.0f0, 9.0f0, 10.0f0, 11.0f0, 12.0f0, 13.0f0, 14.0f0, 15.0f0, 16.0f0, 17.0f0, 18.0f0, 19.0f0, 20.0f0, 21.0f0, 22.0f0, 23.0f0, 24.0f0, 25.0f0, 26.0f0, 27.0f0)
+>julia res = zeros(Float32, 200,200)
+>julia @time res .= polynomial.(Ref(Val(2)), Tuple.(CartesianIndices((200,200))), Ref(cs)); 
+  0.054017 seconds (147.45 k allocations: 10.168 MiB, 99.75% compilation time)
+>julia @time res .= polynomial.(Ref(Val(2)), Tuple.(CartesianIndices((200,200))), Ref(cs)); 
+  0.000089 seconds (3 allocations: 184 bytes)
+```
+"""
+function polynomial(::Val{N}, t::T1, c::T2, ::Val{cstart}=Val(1), ::Val{numvars}=Val(length(t)))::Float32 where {N, NV, TS, T1 <: NTuple{NV, Integer}, T2 <: NTuple{TS, Float32}, cstart, numvars} 
+    c_start = cstart
     res = c[c_start]
     c_start += 1
-    # iterate through the polynomial variables
-    for n in eachindex(t) # 1:length(t) # eachindex(t) # 1:length(t)
-        # subpoly = get_polynomial(Val(n), Val(N-1)); # calulate polynomial with only n variable
-        c_end = c_start + get_num_poly_vars(Val(n), Val(N-1)) - 1
-        # @show N
-        # @show n
-        # @show c_start
-        # @show t
-        # @show c
-        res += t[n] * polynomial(Val(N-1), t[1:n], c[c_start:c_end]) 
-        c_start = c_end + 1
+    # iterate through the polynomial variables: (but this leads to dynamic memory allocation!)
+    # for n = 1:numvars # eachindex(t) # 1:length(t) # eachindex(t) # 1:length(t)
+    #     # c_end = c_start + get_num_poly_vars(Val(n), Val(N-1)) - 1
+    #     res += t[n] * polynomial(Val(N-1), t, c, Val(c_start), Val(n)) 
+    #     c_start += get_num_poly_vars(Val(n), Val(N-1)) # c_end + 1
+    # end
+    # # does not work:
+    # @macroexpand Base.Cartesian.@nexprs 4 n -> begin
+    #     if (numvars >= n)
+    #     res += t[n] * polynomial(Val(N-1), t, c, Val(c_start), Val(n)) 
+    #     c_start += get_num_poly_vars(Val(n), Val(N-1)) # c_end + 1
+    #     end
+    # end
+
+    # this simply unrolls the loop by hand (up to 4D input variables):
+    if numvars >= 1
+        res += t[1] * polynomial(Val(N-1), t, c, Val(c_start), Val(1)) 
+        c_start += get_num_poly_vars(Val(1), Val(N-1)) # c_end + 1
+    end
+    if numvars >= 2
+        res += t[2] * polynomial(Val(N-1), t, c, Val(c_start), Val(2)) 
+        c_start += get_num_poly_vars(Val(2), Val(N-1)) # c_end + 1
+    end
+    if numvars >= 3
+        res += t[3] * polynomial(Val(N-1), t, c, Val(c_start), Val(3)) 
+        c_start += get_num_poly_vars(Val(3), Val(N-1)) # c_end + 1
+    end
+    if numvars >= 4
+        res += t[4] * polynomial(Val(N-1), t, c, Val(c_start), Val(4)) 
+        c_start += get_num_poly_vars(Val(4), Val(N-1)) # c_end + 1
+    end
+    if numvars >= 5
+        error("Only up to 4 dimensions are currently supported for polynomials")
     end
     return res
 end
-#     return mypoly
+
+
+# function unroll_loop(res::Float32, ::Var{0}, ::Var{numvars}) where{n}
+#     return 0f0;
+# end
+
+# function unroll_loop(res::Float32, t, ::Var{c_start},::Var({n}, ::Var{numvars}) where{c_start, n, numvars}
+#     res += t[n] * polynomial(Val(N-1), t, c, Val(c_start), Val(n)) 
+#     c_start += get_num_poly_vars(Val(n), Val(N-1)) # c_end + 1
+#     return unroll_loop(res::Float32, ::Var({n}, ::Var{numvars}) + 
 # end
 
 
@@ -84,13 +121,21 @@ function get_num_multipoly_vars(::Val{numvars}, ::Val{N}) where {numvars, N}
 end
 
 function test_poly_allocations()
-    get_num_poly_vars(Val(2), Val(3)) # 10 indices
-    p = get_polynomial(Val(2), Val(3))  
-    @time p.(Tuple.(CartesianIndices((200,200))),Ref((1.1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27)));
+    # get_num_poly_vars(Val(2), Val(3)) # 10 indices
+    # p = get_polynomial(Val(2), Val(3))  
+    # @time p.(Tuple.(CartesianIndices((200,200))),Ref((1.1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27)));
+
+    polynomial(Val(2), (2f0, 20f0),  (1f0, 1f0, 1f0, 1f0, 1f0, 1f0)) == 467
+
     cids = Tuple.(CartesianIndices((200,200)))
+    # cfds = map((t)->Tuple(Float32.([t...])), cids) 
     cs = Tuple(Float32.([1.1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27]))
-    @time polynomial.(Ref(Val(3)), cids, Ref(cs));
+    res = zeros(Float32, 200,200)
+    @time res .= polynomial.(Ref(Val(2)), cids, Ref(cs));
+    # 0.184371 seconds (2.52 M allocations: 75.226 MiB, 2.59% gc time)
     # does allocate 95 Mb !
+
+    @time polynomial.(Ref(Val(0)), cfds, Ref(cs));
 
     p = get_polynomial(Val(3), Val(2))  # 10 indices required
     get_num_poly_vars(Val(3), Val(2))
