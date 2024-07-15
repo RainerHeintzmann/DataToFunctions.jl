133ca00efb32aab9be0ee1313f1ee2636bf2bb41
hzarei4 <hzoshtolagh@gmail.com>
2024-07-15 13:04:14 +0200

    removed undefined export

--- src/polynomials.jl
+++ src/polynomials.jl
@@ -1,4 +1,4 @@
-export get_polynomial, get_multi_poly, get_num_poly_vars, get_num_multipoly_vars
+export get_multi_poly, get_num_poly_vars, get_num_multipoly_vars
 export polynomial
 
 # using Unrolled
